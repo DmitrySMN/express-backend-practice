02325a0f3d50084703d446f6fedfeb9d906b6d59
dmtr <ddmtry064@gmail.com>
2025-01-26 09:46:15 +0300

    refactor: added required dependencies

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-import express from 'express'
+import express from 'express';
+import {configDotenv} from "dotenv";
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+configDotenv();
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 
-const app = express()
-const port = 3000
+const start = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server started on port = ${port}`);
+    });
+  } catch (ex) {
+    console.log(ex.message);
+  }
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+start();
