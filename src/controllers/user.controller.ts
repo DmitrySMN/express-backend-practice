2fc563edd535eb3718b74ee74ce305b68754dc39
dmtr <ddmtry064@gmail.com>
2025-01-31 17:01:05 +0300

    feat: implemented prisma crud operations

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -10,47 +10,47 @@ export const createUser = async (req: Request, res: Response) => {
   }
 };
 
-// export const getAllUsers = async (req: Request, res: Response) => {
-//   try {
-//     const users = await User.findAll();
-//     res.json(users);
-//   } catch (err: any) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await UserService.findAll();
+    res.json(users);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-// export const getUserById = async (req: Request, res: Response): Promise<any> => {
-//   try {
-//     const user = await User.findById(parseInt(req.params.id));
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-//     res.json(user);
-//   } catch (err: any) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+export const getUserById = async (req: Request, res: Response) => {
+  try {
+    const user = await UserService.findById(parseInt(req.params.id));
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-// export const updateUser = async (req: Request, res: Response): Promise<any> => {
-//   try {
-//     const user = await User.update(parseInt(req.params.id), req.body);
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-//     res.json(user);
-//   } catch (err: any) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const user = await UserService.update(parseInt(req.params.id), req.body);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-// export const deleteUser = async (req: Request, res: Response): Promise<any> => {
-//   try {
-//     const user = await User.delete(parseInt(req.params.id));
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-//     res.json({ message: "User deleted successfully" });
-//   } catch (err: any) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const user = await UserService.delete(parseInt(req.params.id));
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
